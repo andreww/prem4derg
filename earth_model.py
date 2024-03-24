ecacecad8a0dc23fbdc6eeec6b49971121695823
Andrew Walker <andrew.walker@earth.ox.ac.uk>
2024-03-24 15:14:26 +0000

    Flake8 clean

--- earth_model.py
+++ earth_model.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # coding=utf8
-
 """
 Support for PREM-like 1D Earth models
 
@@ -28,10 +27,10 @@ _density_params = np.array([[13.0885,  0.0000, -8.8381,  0.0000],
                             [2.6910,  0.6924,  0.0000,  0.0000],
                             [2.9000,  0.0000,  0.0000,  0.0000],
                             [2.6000,  0.0000,  0.0000,  0.0000]])
-_density_params[:,0] = _density_params[:,0] * 1000.0
-_density_params[:,1] = (_density_params[:,1] * 1000.0) / _r_earth 
-_density_params[:,2] = (_density_params[:,2] * 1000.0) / (_r_earth**2)
-_density_params[:,3] = (_density_params[:,3] * 1000.0) / (_r_earth**3)
+_density_params[:, 0] = _density_params[:, 0] * 1000.0
+_density_params[:, 1] = (_density_params[:, 1] * 1000.0) / _r_earth
+_density_params[:, 2] = (_density_params[:, 2] * 1000.0) / (_r_earth**2)
+_density_params[:, 3] = (_density_params[:, 3] * 1000.0) / (_r_earth**3)
 _vp_params = np.array([[11.2622,   0.0000, -6.3640,   0.0000],
                       [11.0487,  -4.0362,  4.8023, -13.5732],
                       [15.3891,  -5.3181,  5.5242,  -2.5514],
@@ -40,70 +39,70 @@ _vp_params = np.array([[11.2622,   0.0000, -6.3640,   0.0000],
                       [19.0957,  -9.8672,  0.0000,   0.0000],
                       [39.7027, -32.6166,  0.0000,   0.0000],
                       [20.3926, -12.2569,  0.0000,   0.0000],
-                      [ 4.1875,   3.9382,  0.0000,   0.0000],
-                      [ 4.1875,   3.9382,  0.0000,   0.0000],
-                      [ 6.8000,   0.0000,  0.0000,   0.0000],
-                      [ 5.8000,   0.0000,  0.0000,   0.0000]])
-_vs_params = np.array([[ 3.6678,   0.0000,  -4.4475,  0.0000],
-                      [ 0.0000,   0.0000,   0.0000,  0.0000],
-                      [ 6.9254,   1.4672,  -2.0834,  0.9783],
+                      [4.1875,   3.9382,  0.0000,   0.0000],
+                      [4.1875,   3.9382,  0.0000,   0.0000],
+                      [6.8000,   0.0000,  0.0000,   0.0000],
+                      [5.8000,   0.0000,  0.0000,   0.0000]])
+_vs_params = np.array([[3.6678,   0.0000,  -4.4475,  0.0000],
+                      [0.0000,   0.0000,   0.0000,  0.0000],
+                      [6.9254,   1.4672,  -2.0834,  0.9783],
                       [11.1671, -13.7818,  17.4575, -9.2777],
                       [22.3459, -17.2473,  -2.0834,  0.9783],
-                      [ 9.9839,  -4.9324,   0.0000,  0.0000],
+                      [9.9839,  -4.9324,   0.0000,  0.0000],
                       [22.3512, -18.5856,   0.0000,  0.0000],
-                      [ 8.9496,  -4.4597,   0.0000,  0.0000],
-                      [ 2.1519,   2.3481,   0.0000,  0.0000],
-                      [ 2.1519,   2.3481,   0.0000,  0.0000],
-                      [ 3.9000,   0.0000,   0.0000,  0.0000],
-                      [ 3.2000,   0.0000,   0.0000,  0.0000]])
+                      [8.9496,  -4.4597,   0.0000,  0.0000],
+                      [2.1519,   2.3481,   0.0000,  0.0000],
+                      [2.1519,   2.3481,   0.0000,  0.0000],
+                      [3.9000,   0.0000,   0.0000,  0.0000],
+                      [3.2000,   0.0000,   0.0000,  0.0000]])
 # Turn range of polynomials from 0 - 1 to 0 - r_earth
-_vp_params[:,1] = _vp_params[:,1] / _r_earth 
-_vp_params[:,2] = _vp_params[:,2] / (_r_earth**2)
-_vp_params[:,3] = _vp_params[:,3] / (_r_earth**3)
+_vp_params[:, 1] = _vp_params[:, 1] / _r_earth
+_vp_params[:, 2] = _vp_params[:, 2] / (_r_earth**2)
+_vp_params[:, 3] = _vp_params[:, 3] / (_r_earth**3)
 # Turn range of polynomials from 0 - 1 to 0 - r_earth
-_vs_params[:,1] = _vs_params[:,1] / _r_earth 
-_vs_params[:,2] = _vs_params[:,2] / (_r_earth**2)
-_vs_params[:,3] = _vs_params[:,3] / (_r_earth**3)
+_vs_params[:, 1] = _vs_params[:, 1] / _r_earth
+_vs_params[:, 2] = _vs_params[:, 2] / (_r_earth**2)
+_vs_params[:, 3] = _vs_params[:, 3] / (_r_earth**3)
 _q_kappa_params = np.array([1327.7, 57823.0, 57823.0, 57823.0, 57823.0,
                            57823.0, 57823.0, 57823.0, 57823.0, 57823.0,
                            57823.0, 57823.0])
 _q_mu_params = np.array([84.6, np.inf, 312.0, 312.0, 312.0, 143.0, 143.0,
                         143.0, 80.0, 600.0, 600.0, 600.0])
 
+
 class Prem(object):
-    
+
     def __init__(self, breakpoints=_bps, density_params=_density_params,
-                 vp_params=_vp_params, vs_params=_vs_params, 
+                 vp_params=_vp_params, vs_params=_vs_params,
                  q_mu_params=_q_mu_params, q_kappa_params=_q_kappa_params,
                  r_earth=_r_earth):
-        
+
         self.r_earth = r_earth
-        
-        self.density_poly = pp.PeicewisePolynomial(density_params, 
-                                           breakpoints)
+
+        self.density_poly = pp.PeicewisePolynomial(density_params, breakpoints)
         self.vp_poly = pp.PeicewisePolynomial(vp_params, breakpoints)
         self.vs_poly = pp.PeicewisePolynomial(vs_params, breakpoints)
         self.qk_poly = pp.PeicewisePolynomial(q_kappa_params, breakpoints)
         self.qm_poly = pp.PeicewisePolynomial(q_mu_params, breakpoints)
-        
+
         # setup polynomials for mass. This is 4*pi*\int rho(r)*r^2 dr
         r2_params = np.tile(np.array([0.0, 0.0, 1000.0**3]),
-                            (breakpoints.size-1,1))
+                            (breakpoints.size - 1, 1))
         r2_poly = pp.PeicewisePolynomial(r2_params, breakpoints)
         self.mass_poly = self.density_poly.mult(r2_poly)
         self.mass_poly = self.mass_poly.antiderivative()
-        # integrating this gives mass:
+        #  integrating this gives mass:
         self.mass_poly.coeffs = self.mass_poly.coeffs * 4.0 * np.pi
-        
+
         # setup polynomials for MOI. This is 2/3*4*pi*\int rho(r)*r^4 dr
         r4_params = np.tile(np.array([0.0,  0.0, 0.0, 0.0, 1000.0**5]),
-                            (breakpoints.size-1,1))
+                            (breakpoints.size - 1, 1))
         r4_poly = pp.PeicewisePolynomial(r4_params, breakpoints)
-        self.moi_poly = self.density_poly.mult(r4_poly) 
+        self.moi_poly = self.density_poly.mult(r4_poly)
         self.moi_poly = self.moi_poly.antiderivative()
-        # integrating this gives MOI:
-        self.moi_poly.coeffs = self.moi_poly.coeffs * 4.0 * (2/3) * np.pi  
-        
+        #   integrating this gives MOI:
+        self.moi_poly.coeffs = self.moi_poly.coeffs * 4.0 * (2/3) * np.pi
+
     def density(self, r, break_down=False):
         """
         Evaluate density in kg/m**3 at radii r (in km)
@@ -120,9 +119,9 @@ class Prem(object):
         if t != 1:
             qm = self.qm_poly(r, break_down=break_down)
             vs = vs * (1.0 - ((np.log(t)/np.pi)*np.reciprocal(qm)))
-        
+
         return vs
-    
+
     def vp(self, r, t=1, break_down=False):
         """
         Evaluate p-wave velocity (in km/s) at radius r (in km).
@@ -151,7 +150,7 @@ class Prem(object):
         """
         Evaluate bulk modulus (in GPa) at radius r (in km)
         """
-        vp = self.vp_poly(r) * 1000.0 # m/s
+        vp = self.vp_poly(r) * 1000.0  # m/s
         mu = self.shear_modulus(r)
         density = self.density_poly(r)
         return ((vp**2 * density) / 1e9) - mu
@@ -160,29 +159,29 @@ class Prem(object):
         """
         Evaluate shear modulus (in GPa) at radius r (in km)
         """
-        vs = self.vs_poly(r) * 1000.0 # m/s
+        vs = self.vs_poly(r) * 1000.0  # m/s
         density = self.density_poly(r)
-        return (vs**2 * density) / 1e9     
+        return (vs**2 * density) / 1.0e9
 
     def mass(self, r, r_inner=0.0):
         """
         Evaluate mass inside radius r (in km)
         """
         if np.ndim(r) == 0:
-            m = self.mass_poly.integrate(r_inner,r)
+            m = self.mass_poly.integrate(r_inner, r)
         else:
             m = np.zeros_like(r)
             for i in range(r.size):
                 if r[i] == 0:
                     m[i] = 0
                 else:
-                    m[i] = self.mass_poly.integrate(r_inner,r[i])
+                    m[i] = self.mass_poly.integrate(r_inner, r[i])
         return m
-    
+
     def moment_or_inertia(self, r, r_inner=0.0):
         """
         Evaluate moment of inertia inside radius r (in km)
-        
+
         Return a tuple of moment of inertia (in kg m^2) and
         the moment of inertia factor (I/MR**2, dimensionless)
         which is 0.4 for a uniform density body, and decreases
@@ -190,27 +189,27 @@ class Prem(object):
 
         """
         if np.ndim(r) == 0:
-            moi = self.moi_poly.integrate(r_inner,r)
+            moi = self.moi_poly.integrate(r_inner, r)
         else:
             moi = np.zeros_like(r)
             for i in range(r.size):
                 if r[i] == 0:
                     moi[i] = 0
                 else:
-                    moi[i] = self.moi_poly.integrate(r_inner,r[i])
-                    
+                    moi[i] = self.moi_poly.integrate(r_inner, r[i])
+
         r_in_m = r * 1000
         m = self.mass(r)
         moif = moi / (m*(r_in_m**2))
         return moi, moif
-    
+
     def gravity(self, r):
         """
         Evaluate acceleration due to gravity at radius r in m/s^2
         """
         G = 6.6743E-11
         if np.ndim(r) == 0:
-            g = self.mass_poly.integrate(0.0,r)/((r*1000)**2)*G
+            g = self.mass_poly.integrate(0.0, r)/((r*1000)**2)*G
         else:
             g = np.zeros_like(r)
             for i in range(r.size):
@@ -218,16 +217,16 @@ class Prem(object):
                     g[i] = 0
                 else:
                     g[i] = self.mass_poly.integrate(0.0, r[i]) / \
-                                                      ((r[i]*1000)**2)*G 
+                                                      ((r[i]*1000)**2)*G
         return g
-    
+
     def grav_potential(self, r):
         """
         Evaluate the gravitational potential at radius r in J/kg
         """
         G = 6.6743E-11
         if np.ndim(r) == 0:
-            phi = -1 * self.mass_poly.integrate(0.0,r)/(r*1000)*G
+            phi = -1 * self.mass_poly.integrate(0.0, r)/(r*1000)*G
         else:
             phi = np.zeros_like(r)
             for i in range(r.size):
@@ -235,15 +234,15 @@ class Prem(object):
                     phi[i] = 0
                 else:
                     phi[i] = -1 * self.mass_poly.integrate(0.0, r[i]) / \
-                                                          (r[i]*1000)*G 
+                                                          (r[i]*1000)*G
         return phi
-    
+
     def pressure(self, r):
         """
         Evaluate pressure (in GPa) at radius r (in km)
         """
         # NB: this is done numerically, because otherise
-        # I need to work out how to express g as a polynomial,
+        # I need to work out how to express g as a polynomial,
         # multiply this by rho, then do the integral inwards.
         # all a bit of a faff!
         if np.ndim(r) == 0:
@@ -251,7 +250,7 @@ class Prem(object):
             rs = np.arange(r, self.r_earth, 1.0)
             g = self.gravity(rs)
             rho = self.density(rs)
-            ps = spint.cumtrapz((-g*rho)[::-1],rs[::-1]*1000.0, initial=0)
+            ps = spint.cumtrapz((-g*rho)[::-1], rs[::-1]*1000.0, initial=0)
             pressure = ps[-1]/1E9
         else:
             # Assume I have been fed something I can integrate
@@ -269,7 +268,7 @@ class Prem(object):
         This method creates a numpy record array with the model evaulated
         at all depths with a minimum spacing of min_step km. All breakpoints
         are also included in the output. If the densioty is discontinuoius,
-        the depth is represented twice, first with the value above the 
+        the depth is represented twice, first with the value above the
         discontiuity, then with the value below it. This representation can
         be used to construct travel time curves (for examople).
 
@@ -293,18 +292,18 @@ class Prem(object):
         qks = np.array([])
         qms = np.array([])
 
-        nbps = len(self.density_poly.breakpoints) -1
+        nbps = len(self.density_poly.breakpoints) - 1
         for i in range(nbps):
             j = nbps - i
             k = j - 1
-            rs = np.arange(self.density_poly.breakpoints[j], 
+            rs = np.arange(self.density_poly.breakpoints[j],
                            self.density_poly.breakpoints[k], -min_step)
             ds = self.r_earth - rs
-            dens = self.density(rs, break_down=True) # As we go inwards
-            vp = self.vp(rs, break_down=True) # As we go inwards
-            vs = self.vs(rs, break_down=True) # As we go inwards
-            qk = self.qkappa(rs, break_down=True) # As we go inwards
-            qm = self.qshear(rs, break_down=True) # As we go inwards
+            dens = self.density(rs, break_down=True)  # As we go inwards
+            vp = self.vp(rs, break_down=True)  # As we go inwards
+            vs = self.vs(rs, break_down=True)  # As we go inwards
+            qk = self.qkappa(rs, break_down=True)  # As we go inwards
+            qm = self.qshear(rs, break_down=True)  # As we go inwards
             radii = np.append(radii, rs)
             depths = np.append(depths, ds)
             densities = np.append(densities, dens)
@@ -312,10 +311,10 @@ class Prem(object):
             vss = np.append(vss, vs)
             qks = np.append(qks, qk)
             qms = np.append(qms, qm)
-        
-            # Look at the breakpoint. If it is discontinous in 
-            # vJalue put add it here (i.e. so we have above followed 
-            # by below for the next step). Othersie we can skip it 
+
+            # Look at the breakpoint. If it is discontinous in
+            # value put add it here (i.e. so we have above followed
+            # by below for the next step). Othersie we can skip it
             # (and it gets adder in the next iteration). But we need
             # to hadle k = 0 carefully (always stick in the origin)
             if k == 0:
@@ -329,13 +328,13 @@ class Prem(object):
                 qm = self.qshear(rs)
                 radii = np.append(radii, rs)
                 depths = np.append(depths, ds)
-                densities = np.append(densities, dens) 
+                densities = np.append(densities, dens)
                 vps = np.append(vps, vp)
                 vss = np.append(vss, vs)
                 qks = np.append(qks, qk)
                 qms = np.append(qms, qm)
-            elif (self.density(self.density_poly.breakpoints[k]) != 
-                  self.density(self.density_poly.breakpoints[k], 
+            elif (self.density(self.density_poly.breakpoints[k]) !=
+                  self.density(self.density_poly.breakpoints[k],
                                break_down=True)):
                 # Add the value above the inner boundary of this layer
                 rs = self.density_poly.breakpoints[k]
@@ -352,10 +351,11 @@ class Prem(object):
                 vss = np.append(vss, vs)
                 qks = np.append(qks, qk)
                 qms = np.append(qms, qm)
-            
-        result = np.core.records.fromarrays([depths, radii, densities,
-                                             vps, vss, qks, qms], 
-                      names='depth, radius, density, vp, vs, qkappa, qshear')
+
+        result = np.core.records.fromarrays(
+            [depths, radii, densities, vps, vss, qks, qms],
+            names='depth, radius, density, vp, vs, qkappa, qshear'
+            )
         return result
 
     def tabulate_model_outwards(self, min_step):
@@ -365,7 +365,7 @@ class Prem(object):
         This method creates a numpy record array with the model evaulated
         at all depths with a minimum spacing of min_step km. All breakpoints
         are also included in the output. If the densioty is discontinuoius,
-        the depth is represented twice, first with the value above the 
+        the depth is represented twice, first with the value above the
         discontiuity, then with the value below it. This representation can
         be used to construct travel time curves (for examople).
 
@@ -393,7 +393,7 @@ class Prem(object):
         for i in range(nbps):
             j = i
             k = j + 1
-            rs = np.arange(self.density_poly.breakpoints[j], 
+            rs = np.arange(self.density_poly.breakpoints[j],
                            self.density_poly.breakpoints[k], min_step)
             ds = self.r_earth - rs
             dens = self.density(rs)
@@ -408,10 +408,10 @@ class Prem(object):
             vss = np.append(vss, vs)
             qks = np.append(qks, qk)
             qms = np.append(qms, qm)
-        
-            # Look at the breakpoint. If it is discontinous in 
-            # vJalue put add it here (i.e. so we have above followed 
-            # by below for the next step). Othersie we can skip it 
+
+            # Look at the breakpoint. If it is discontinous in
+            # value put add it here (i.e. so we have above followed
+            # by below for the next step). Othersie we can skip it
             # (and it gets adder in the next iteration). But we need
             # to hadle k = 0 carefully (always stick in the origin)
             if k == nbps + 1:
@@ -425,13 +425,13 @@ class Prem(object):
                 qm = self.qshear(rs)
                 radii = np.append(radii, rs)
                 depths = np.append(depths, ds)
-                densities = np.append(densities, dens) 
+                densities = np.append(densities, dens)
                 vps = np.append(vps, vp)
                 vss = np.append(vss, vs)
                 qks = np.append(qks, qk)
                 qms = np.append(qms, qm)
-            elif (self.density(self.density_poly.breakpoints[k]) != 
-                  self.density(self.density_poly.breakpoints[k], 
+            elif (self.density(self.density_poly.breakpoints[k]) !=
+                  self.density(self.density_poly.breakpoints[k],
                                break_down=True)):
                 # Add the value above the inner boundary of this layer
                 rs = self.density_poly.breakpoints[k]
@@ -448,8 +448,9 @@ class Prem(object):
                 vss = np.append(vss, vs)
                 qks = np.append(qks, qk)
                 qms = np.append(qms, qm)
-            
-        result = np.core.records.fromarrays([depths, radii, densities,
-                                             vps, vss, qks, qms], 
-                      names='depth, radius, density, vp, vs, qkappa, qshear')
+
+        result = np.core.records.fromarrays(
+            [depths, radii, densities, vps, vss, qks, qms],
+            names='depth, radius, density, vp, vs, qkappa, qshear'
+            )
         return result
--- peice_poly.py
+++ peice_poly.py
@@ -6,34 +6,34 @@ Peicewise polynomials like PREM
 """
 import numpy as np
 
+
 class PeicewisePolynomial(object):
     """
     Peicewise Polynomials a different way
-    
+
     The SciPy PPoly class defines a function from
     polynomials with coefficents c and breakpoints x
     evaluated at a point xp thus:
-    
+
        S = sum(c[m, i] * (xp - x[i])**(k-m) for m in range(k+1))
-    
+
     This is not helpful for PREM, so we create a new class defining
     the function:
-    
+
        S = sum(c[m, i] * (xp - x[i])**(k-m) for m in range(k+1))
-      
+
     Note some important differences between this and PPoly!
     """
-    
+
     def __init__(self, c, x):
-        assert len(x.shape)==1, "breakpoints must be 1D"
-        self.breakpoints = x 
-        if len(c.shape)==1:
+        assert len(x.shape) == 1, "breakpoints must be 1D"
+        self.breakpoints = x
+        if len(c.shape) == 1:
             c = np.expand_dims(c, axis=1)
-            c = np.append(c,np.zeros_like(c), axis=1)
-        assert len(c.shape)==2, "breakpoints must be 2D"
+            c = np.append(c, np.zeros_like(c), axis=1)
+        assert len(c.shape) == 2, "breakpoints must be 2D"
         self.coeffs = c
-        
-        
+
     def __call__(self, xp, break_down=False):
         if np.ndim(xp) == 0:
             value = self._evaluate_at_point(xp, break_down)
@@ -41,9 +41,8 @@ class PeicewisePolynomial(object):
             value = np.zeros_like(xp)
             for i in range(xp.size):
                 value[i] = self._evaluate_at_point(xp[i], break_down)
-        return value        
-        
-        
+        return value
+
     def _evaluate_at_point(self, x, break_down=False):
         """
         Evaluate peicewise polynomal at point x
@@ -51,92 +50,84 @@ class PeicewisePolynomial(object):
         coef = self._get_coefs(x, break_down)
         value = self._evaluate_polynomial(x, coef)
         return value
-        
-        
+
     def _evaluate_polynomial(self, x, coef):
         value = 0
         for i, c in enumerate(coef):
             value = value + c * x**i
         return value
-        
-        
+
     def _get_coefs(self, x, break_down=False):
         """
         Return coefs at x
-        
-        If x falls on a breakpoint, we take the coeffecents from 
+
+        If x falls on a breakpoint, we take the coeffecents from
         'above' the breakpoint. Unless break_down is True, in which
         case we take the coeffecents from 'below'
         """
         if x == self.breakpoints[-1]:
             # We use the last coefficents for the outside point
-            return self.coeffs[-1,:]
+            return self.coeffs[-1, :]
         if break_down:
             for i in range(self.breakpoints.size):
-                if ((x > self.breakpoints[i]) and 
-                              (x <= self.breakpoints[i+1])):
-                    return self.coeffs[i,:]
+                if ((x > self.breakpoints[i])
+                   and (x <= self.breakpoints[i+1])):
+                    return self.coeffs[i, :]
         else:
             for i in range(self.breakpoints.size):
-                if ((x >= self.breakpoints[i]) and 
-                               (x < self.breakpoints[i+1])):
-                    return self.coeffs[i,:]
-        
+                if ((x >= self.breakpoints[i])
+                   and (x < self.breakpoints[i+1])):
+                    return self.coeffs[i, :]
+
         return None
-    
-    
+
     def derivative(self):
-    
         deriv_breakpoints = self.breakpoints
         deriv_coeffs = np.zeros((self.coeffs.shape[0],
                                  self.coeffs.shape[1]-1))
         for seg in range(self.coeffs.shape[0]):
             for i in range(self.coeffs.shape[1]):
                 if i == 0:
-                    continue # Throw away term for x**0
-                deriv_coeffs[seg,i-1] = self.coeffs[seg,i]*i
-                
+                    continue  # Throw away term for x**0
+                deriv_coeffs[seg, i-1] = self.coeffs[seg, i]*i
+
         deriv = PeicewisePolynomial(deriv_coeffs, deriv_breakpoints)
         return deriv
-    
+
     def antiderivative(self):
-        
         antideriv_breakpoints = self.breakpoints
         antideriv_coeffs = np.zeros((self.coeffs.shape[0],
                                      self.coeffs.shape[1]+1))
         for seg in range(self.coeffs.shape[0]):
             for i in range(self.coeffs.shape[1]):
-                antideriv_coeffs[seg,i+1] = self.coeffs[seg,i]/(i+1)
-                
-        antideriv = PeicewisePolynomial(antideriv_coeffs, 
+                antideriv_coeffs[seg, i+1] = self.coeffs[seg, i]/(i+1)
+
+        antideriv = PeicewisePolynomial(antideriv_coeffs,
                                         antideriv_breakpoints)
         return antideriv
-    
-    
+
     def integrate(self, a, b):
-        
-        #antiderivative = self.antiderivative()
+        # antiderivative = self.antiderivative()
         integral = 0
         lower_bound = a
         for bpi, bp in enumerate(self.breakpoints):
             if bp > lower_bound:
                 if self.breakpoints[bpi] >= b:
                     # Just the one segment left - add it and end
-                    integral = integral + (self(b, break_down=True) - 
+                    integral = integral + (self(b, break_down=True) -
                                            self(lower_bound))
-                    #print(integral, lower_bound, b, 'done')
+                    # print(integral, lower_bound, b, 'done')
                     break
                 else:
                     # segment from lower bound to bp
                     # add it, increment lower_bound and contiue
-                    integral = integral + (self(bp, break_down=True) - 
+                    integral = integral + (self(bp, break_down=True) -
                                            self(lower_bound))
-                    #print(integral, lower_bound, bp)
+                    # print(integral, lower_bound, bp)
                     lower_bound = bp
 
         return integral
-    
-    
+
     def mult(self, other):
         assert self.coeffs.shape[0] == other.coeffs.shape[0], \
                                      'different number of breakpoints'
@@ -146,8 +137,8 @@ class PeicewisePolynomial(object):
         for seg in range(self.coeffs.shape[0]):
             for i in range(self.coeffs.shape[1]):
                 for j in range(other.coeffs.shape[1]):
-                    mult_coefs[seg,i+j] = mult_coefs[seg,i+j] + \
-                                 self.coeffs[seg,i] * other.coeffs[seg,j]
-                    
+                    mult_coefs[seg, i+j] = mult_coefs[seg, i+j] + \
+                                 self.coeffs[seg, i] * other.coeffs[seg, j]
+
         mult_poly = PeicewisePolynomial(mult_coefs, mult_breakpoints)
         return mult_poly
